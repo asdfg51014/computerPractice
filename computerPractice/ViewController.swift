ccbfd06399103cfec058ca9b2169660830fdabb6
Al.C <al.c@alcde-macbook-air.local>
2018-07-24 13:29:44 +0800

    Initial Commit

---
+++ computerPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  computerPractice
+//
+//  Created by Al.C on 2018/7/24.
+//  Copyright © 2018 Al.C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
